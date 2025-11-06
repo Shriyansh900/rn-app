c236fbddd8c5729efb18ebe273d7c0ff4003e7a2
Shriyansh Ozarkar <sozarkar7@gmail.com>
2025-11-07 01:59:20 +0530

    first React Native app Start

---
+++ app/index.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center h-screen  ">
+      <Text className="bg-black text-white ">Hello Shriyansh Bhai 🙋</Text>
+    </View>
+  );
+}
